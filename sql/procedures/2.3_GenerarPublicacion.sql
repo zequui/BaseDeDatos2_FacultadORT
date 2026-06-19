f4ee54c8064f761df952dc57e2d6bfa6b58eb771
JuanfraChaves <chjuanfra@gmail.com>
2026-06-19 13:11:53 -0300

    segundo intento

---
+++ sql/procedures/2.3_GenerarPublicacion.sql
@@ -0,0 +1,37 @@
+create or replace PROCEDURE generarPublicacion(
+    p_id_agente NUMBER,
+    p_id_comunidad NUMBER,
+    p_titulo VARCHAR2,
+    p_contenido VARCHAR2
+)
+AS
+    v_id_contenido NUMBER;
+BEGIN
+
+    INSERT INTO CONTENIDO(
+        fechaCreacion,
+        horaCreacion,
+        id_agente,
+        id_comunidad
+    )
+    VALUES(
+        SYSDATE,
+        TO_CHAR(SYSDATE,'HH24:MI:SS'),
+        p_id_agente,
+        p_id_comunidad
+    )
+    RETURNING id INTO v_id_contenido;
+
+    INSERT INTO PUBLICACION(
+        id,
+        titulo,
+        contenido
+    )
+    VALUES(
+        v_id_contenido,
+        p_titulo,
+        p_contenido
+    );
+
+END;
+/
