7c51b7001b80542a228922e7b32e586de1c4d475
JuanfraChaves <chjuanfra@gmail.com>
2026-06-22 08:42:56 -0300

    feat: req5.3

---
+++ mongoDB/queries/5.3_InteraccionesXHora.js
@@ -0,0 +1,43 @@
+db.eventos.aggregate([
+{
+    $match: {
+        agente_id: 5,
+        tipo_evento: "interaccion_usuario"
+    }
+},
+{
+    $addFields: {
+        hora: {
+            $hour: "$fecha"
+        }
+    }
+},
+{
+    $match: {
+        hora: {
+            $gte: 8,
+            $lte: 17
+        }
+    }
+},
+{
+    $group: {
+        _id: "$hora",
+        cantidad_interacciones: {
+            $sum: 1
+        }
+    }
+},
+{
+    $sort: {
+        _id: 1
+    }
+},
+{
+    $project: {
+        _id: 0,
+        hora: "$_id",
+        cantidad_interacciones: 1
+    }
+}
+])
